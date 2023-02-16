c478df57d5e84675af28b571880c406b9073b44b
Herman <chentt_ro@icloud.com>
2023-02-16 10:53:24 +0800

    add: add search query by category

--- a1homework/app.js
+++ a1homework/app.js
@@ -11,25 +11,24 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
-//嘗試把CSS分離看看
 app.get('/', (req, res) => {
-  res.render('index', { css: 'index.css', restaurants: restaurantList.results })
+  res.render('index', { restaurants: restaurantList.results })
 })
 
 app.get('/restaurants/:number', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.number)
-  res.render('show', { css: 'show.css', restaurant })
+  res.render('show', { restaurant })
 })
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase()
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword)
-  })
+  const restaurants = restaurantList.results.filter(restaurant =>
+    restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+  )
   if (restaurants.length === 0) {
-    res.render('noSearchResults', { css: 'index.css' })
+    res.render('noSearchResults')
   } else {
-    res.render('index', { css: 'index.css', restaurants })
+    res.render('index', { restaurants })
   }
 
 })
