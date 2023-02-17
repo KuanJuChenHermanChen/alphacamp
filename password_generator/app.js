f30b0499f9bd9ad8cc5a9de4891f8bd738d77ea2
Herman <chentt_ro@icloud.com>
2023-02-17 18:35:33 +0800

    end

--- password_generator/app.js
+++ password_generator/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const generatePassword = require('./generate_password')
 const app = express()
 const port = 3000
 
@@ -16,7 +17,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  console.log(`body-parser: ${JSON.stringify(req.body)}`)
+  const options = req.body
+  const password = generatePassword(req.body)
+  res.render('index', { password, options })
 })
 
 // starts the express server and listening for connections.
--- password_generator/generate_password.js
+++ password_generator/generate_password.js
@@ -1,48 +1,57 @@
-// generate_password.js
+// define sample function to randomly return an item in an array
+function sample(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
 
 // define generatePassword function
-function generatePassword() {
+function generatePassword(options) {
   // define things user might want
   const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
   const upperCaseLetters = lowerCaseLetters.toUpperCase()
   const numbers = '1234567890'
   const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
 
-  // define dummy data
-  const options = {
-    length: 12,
-    lowercase: 'on',
-    uppercase: 'on',
-    numbers: 'on',
-    excludeCharacters: '40'
-  }
-
   // create a collection to store things user picked up
   let collection = []
 
   if (options.lowercase === 'on') {
-    collection = collection.concat([...lowerCaseLetters])
+    collection = collection.concat(lowerCaseLetters.split(''))
   }
 
   if (options.uppercase === 'on') {
-    collection = collection.concat([...upperCaseLetters])
+    collection = collection.concat(upperCaseLetters.split(''))
   }
 
   if (options.numbers === 'on') {
-    collection = collection.concat([...numbers])
+    collection = collection.concat(numbers.split(''))
   }
 
   if (options.symbols === 'on') {
-    collection = collection.concat([...symbols])
+    collection = collection.concat(symbols.split(''))
   }
 
-  console.log('collection', collection)
   // remove things user do not need
+  if (options.excludeCharacters) {
+    collection = collection.filter(
+      character => !options.excludeCharacters.includes(character)
+    )
+  }
+
+  // return error notice if collection is empty
+  if (collection.length === 0) {
+    return 'There is no valid character in your selection.'
+  }
 
   // start generating password
+  let password = ''
+  for (let i = 0; i < Number(options.length); i++) {
+    password += sample(collection)
+  }
 
   // return the generated password
-  console.log('This function will generate password')
+  return password
 }
-// invoke generatePassword function 
-generatePassword()
+
+// export generatePassword function for other files to use
+module.exports = generatePassword
