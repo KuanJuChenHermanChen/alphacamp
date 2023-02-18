64647653976833dae92a8b3dd310f0dbd73f8aa2
Herman <chentt_ro@icloud.com>
2023-02-18 23:56:19 +0800

    feat: add detail page

--- todoList/app.js
+++ todoList/app.js
@@ -59,6 +59,17 @@ app.post('/todos', async (req, res, next) => {
   } 
 })
 
+app.get('/todos/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const todo = await Todo.findById(id)
+    res.render('detail', { todo })
+  } catch (err) {
+    console.error(err)
+    next(err);
+  }
+})
+
 app.listen(port, (req, res) => {
   console.log('start on 3000')
 })
