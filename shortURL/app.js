b2130ceaef61a0db0571ee095f30c5978eeb93a3
Herman <chentt_ro@icloud.com>
2023-02-28 03:15:47 +0800

    add: add controller and import in app.js

--- shortURL/app.js
+++ shortURL/app.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const { generateShortUrl } = require('./controller/shorturl')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
+
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  generateShortUrl(req.body.url)
 })
 
 app.listen(port, () => {
